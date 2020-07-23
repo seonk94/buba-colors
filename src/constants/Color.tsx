ebf5cc48108c416c68421d7899ce8c699cfb619f
seonk94 <seonk94@naver.com>
2020-07-23 22:05:21 +0900

    feat: color constants

---
+++ src/constants/Color.tsx
@@ -0,0 +1,12 @@
+export const secondary = {
+  main: '#1c7ed6',
+  light: '#74c0fc',
+  dark: '#1864ab',
+}
+
+export const primary = {
+  main: '#495057',
+  light: '#dee2e6',
+  dark: '#212529',
+}
+
