87606333a48347530f24d0d54d5f8bc2e91f6a40
seonk94 <seonk94@naver.com>
2020-07-24 22:03:32 +0900

    fix: button story

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -6,12 +6,12 @@ export default {
   component: Button,
 }
 
-export const primaryButton = () => <div>
+export const buttonStory = () => <div>
+  <h5>Primary Button</h5>
   <Button theme='primary'>button</Button>
   <Button theme='primary' outlined>button</Button>
   <Button theme='primary' flat>button</Button>
-</div>;
-export const secondaryButton = () => <div>
+  <h5>Sceondary Button</h5>
   <Button theme='secondary'>button</Button>
   <Button theme='secondary' outlined>button</Button>
   <Button theme='secondary' flat>button</Button>
